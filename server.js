c0560a9b223f47276032af24f701fe2efc308284
Joshua Wilensky <joshwilensky@gmail.com>
2020-07-31 17:36:34 -0400

    added midleware, get api

--- server.js
+++ server.js
@@ -7,11 +7,29 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// SETS UP THE EXPRESS APP TO HANDLE DATA PARSING---------
+// SETS UP THE EXPRESS APP TO HANDLE DATA PARSING
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Starts the server to begin listening---------------
+// MIDDLEWARE ACTIVATION:
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// GET API NOTES:
+app.get("/api/notes", function (req, res) {
+  fs.readFile("./db/db.json", function (err, data) {
+    var note = JSON.parse(data);
+    res.json(note);
+    console.log(note);
+  });
+});
+
+// STARTS THE SERVER TO BEGIN LISTENING---------------
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
